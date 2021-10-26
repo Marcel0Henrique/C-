c4da2245c3aea8b1284465051ef942dada6db0cb
Marcel0Henrique <marcelomonteiro1275@gmail.com>
2021-10-26 07:10:45 -0300

    first commit

---
+++ Exercicios/Calculadora/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*CALCULADORA*");
+
+            System.Console.WriteLine();
+
+            int n1 = int.Parse(Console.ReadLine("Digite o 1° Numero:"));
+        }
+    }
+}
